279a0c3899679a2d46e5db13767fe7cee4933130
slzatz <slzatz@gmail.com>
2021-02-15 08:29:11 -0500

    editor.go, listman.go: continued porting from cpp to go

--- editor.go
+++ editor.go
@@ -8,19 +8,199 @@ import (
 
 type eMode int
 
-const (
-	insert eMode = iota
-	normal
-	comandLine
-)
+const SMARTINDENT = 4; //should be in config
 
-type editor struct {
+type Editor struct {
+    cx, cy  int//cursor x and y position
+    fc, fr int// file x and y position
+    line_offset int//row the user is currently scrolled to
+    prev_line_offset int
+    coloff  int//column user is currently scrolled to
+    screenlines int //number of lines for this Editor
+    screencols int  //number of columns for this Editor
+    left_margin int //can vary (so could TOP_MARGIN - will do that later
+    left_margin_offset int // 0 if no line numbers
+    top_margin int
+    rows []string
+    code  string//used by lsp thread and intended to avoid unnecessary calls to editorRowsToString
+    dirty int //file changes since last save
+    highlight [2]int
+    vb0 [3]int
+    mode int
+    command_line string //for commands on the command line; string doesn't include ':'
+    command string // right now includes normal mode commands and command line commands
+    last_command string
+    repeat int
+    last_repeat int
+    prev_fr, prev_fc int
+    //what's typed between going into INSERT mode and leaving INSERT mode
+    last_typed string
+    indent int
+    smartindent int
+    first_visible_row int
+    last_visible_row int
+    spellcheck bool
+    highlight_syntax bool
+    redraw bool
+    pos_mispelled_words [][2]int
+    message string//status msg is a character array max 80 char
+    string_buffer string//yanking chars
+    line_buffer []string
+    //static int total_screenlines; //total screenlines available to Editors vertically
+    //static int origin; //x column of Editor section
+    search_string string //word under cursor works with *, n, N etc.
+    id int//listmanager db id of the row
+    undo_deque []Diff//if neg it was a delete
+    d_index int//undo_deque index
+    undo_mode bool
+    snapshot []string
+    linked_editor *Editor
+    is_subeditor bool
+    is_below bool
+    //nuspell::Dictionary dict;
+  }
+
+func NewEditor() *Editor {
+	return &Editor{
+      cx: 0, //actual cursor x position (takes into account any scroll/offset)
+      cy: 0, //actual cursor y position ""
+      fc: 0, //'file' x position as defined by reading sqlite text into rows vector
+      fr: 0, //'file' y position ""
+      line_offset: 0,  //the number of lines of text at the top scrolled off the screen
+      prev_line_offset: 0,  //the prev number of lines of text at the top scrolled off the screen
+      //E.coloff: 0,  //always zero because currently only word wrap supported
+      dirty: 0, //has filed changed since last save
+      message[0]: '\0', //very bottom of screen, ex. -- INSERT --
+      highlight = [2]{-1,-1},
+      mode: 0, //0=normal, 1=insert, 2=command line, 3=visual line, 4=visual, 5='r' 
+      command: "",
+      command_line: "",
+      repeat: 0, //number of times to repeat commands like x,s,yy also used for visual line mode x,y
+      indent: 4,
+      smartindent: 1, //CTRL-z toggles - don't want on what pasting from outside source
+      first_visible_row: 0,
+      spellcheck: false,
+      highlight_syntax: true, // should only apply to code
+      redraw: false,
+      undo_mode: false,
+      linked_editor: nil,
+      is_subeditor: false,
+      is_below: false,
+      left_margin_offset: 0, // 0 if no line numbers
+
+      /*
+      auto dict_list: std::vector<std::pair<std::string, std::string>>{},
+      nuspell::search_default_dirs_for_dicts(dict_list),
+      auto dict_name_and_path: nuspell::find_dictionary(dict_list, "en_US"),
+      auto & dict_path: dict_name_and_path->second,
+      dict: nuspell::Dictionary::load_from_path(dict_path),
+      */
+    }
+}
+
+// cmd1_map = make(map[string]func(*Editor, int),4)
+cmd_map1 := map[string]func(*Editor, int){
+                   "i":(*Editor).E_i,
+                   "I":(*Editor).E_a,
+                   "a":(*Editor).E_a,
+                   "A":(*Editor).E_A,
+                 }
+// to call it's cmd1_map["i"](e, repeat)
+
+cmd_map2 := map[string]func(*Editor, int){
+                   "o":(*Editor).E_o_escape,
+                   "O":(*Editor).E_O_escape,
+                 }
+
+cmd_map3 := map[string]func(*Editor, int){
+                   "x":(*Editor).E_x,
+                   "dw":(*Editor).E_dw,
+                   "daw":(*Editor).E_daw,
+                   "dd":(*Editor).E_dd,
+                   "d$":(*Editor).E_d$,
+                   "de":(*Editor).E_de,
+                   "dG":(*Editor).E_dG,
+                 }
+
+cmd_map4 := map[string]func(*Editor, int){
+                   "cw":(*Editor).E_cw,
+                   "caw":(*Editor).E_caw,
+                   "s":(*Editor).E_s,
+                   "A":(*Editor).E_A,
+                 }
+
+    void setLinesMargins(void);
+    bool find_match_for_left_brace(char, bool back=false);
+    std::pair<int,int> move_to_right_brace(char);
+    bool find_match_for_right_brace(char, bool back=false);
+    std::pair<int,int> move_to_left_brace(char);
+    void draw_highlighted_braces(void);
+    //void position_editors(void); in session struct
+    
+
+
+
+
+
+
+
+
+
+
+
+
 	mode eMode
 	rows []Erow
 }
 
+type Diff struct {
+  fr int
+  fc int
+  repeat int
+  command string
+  rows []string
+  num_rows int//the row where insert occurs counts 1 and then any rows added with returns
+  inserted_text string
+  deleted_text string //deleted chars - being recorded by not used right now or perhaps ever!
+  diff []struct{byte, int}
+  changed_rows []struct{int, string}
+  undo_method int//CHANGE_ROW< REPLACE_NOTE< ADD_ROWS, DELETE_ROWS
+  mode int
+};
+
 // ERow represents a line of text in a file
 type ERow []rune
+
+// cmd1_map = make(map[string]func(*Editor, int),4)
+cmd1_map1 := map[string]func(*Editor, int){
+                   "i":(*Editor).E_i,
+                   "I":(*Editor).E_a,
+                   "a":(*Editor).E_a,
+                   "A":(*Editor).E_A,
+                 }
+// to call it's cmd1_map["i"](e, repeat)
+
+cmd1_map2 := map[string]func(*Editor, int){
+                   "o":(*Editor).E_o_escape,
+                   "O":(*Editor).E_O_escape,
+                 }
+
+cmd1_map3 := map[string]func(*Editor, int){
+                   "x":(*Editor).E_x,
+                   "dw":(*Editor).E_dw,
+                   "daw":(*Editor).E_daw,
+                   "dd":(*Editor).E_dd,
+                   "d$":(*Editor).E_d$,
+                   "de":(*Editor).E_de,
+                   "dG":(*Editor).E_dG,
+                 }
+
+cmd1_map4 := map[string]func(*Editor, int){
+                   "cw":(*Editor).E_cw,
+                   "caw":(*Editor).E_caw,
+                   "s":(*Editor).E_s,
+                   "A":(*Editor).E_A,
+                 }
 
 // Text expands tabs in an eRow to spaces
 func (row ERow) Text() ERow {
@@ -100,207 +280,3 @@ func NewEditorFromFile(filename string) (*Editor, error) {
 	}, nil
 }
 
-// CursorUp moves the cursor up as long as row number is non zero
-func (e *Editor) CursorUp() {
-	if e.Cursor.Row > 0 {
-		e.Cursor.Row--
-	}
-	e.ResetX()
-}
-
-// CursorDown moves the cursor down till one line past the last line
-func (e *Editor) CursorDown() {
-	if e.Cursor.Row < len(e.Rows) {
-		e.Cursor.Row++
-	}
-	e.ResetX()
-}
-
-// CursorLeft moves the cursor left. If at col 0 & any line other thant
-// the first line, it moves to the previous line
-func (e *Editor) CursorLeft() {
-
-	if e.Cursor.Col > 0 {
-		e.Cursor.Col--
-	} else if e.Cursor.Row > 0 {
-		e.Cursor.Row--
-		e.Cursor.Col = len(e.Rows[e.Cursor.Row])
-	}
-}
-
-// CursorRight moves the cursor right & wraps past EOL to col 0
-func (e *Editor) CursorRight() {
-	// right moves only if we're within a valid line.
-	// for past EOF, there's no movement
-	if e.Cursor.Row >= len(e.Rows) {
-		return
-	}
-	if e.Cursor.Col < len(e.Rows[e.Cursor.Row]) {
-		e.Cursor.Col++
-	} else if e.Cursor.Col == len(e.Rows[e.Cursor.Row]) {
-		e.Cursor.Row++
-		e.Cursor.Col = 0
-	}
-}
-
-// CursorEnd moves the cursor to end of line
-func (e *Editor) CursorEnd() {
-	if e.Cursor.Row < len(e.Rows) {
-		e.Cursor.Col = len(e.Rows[e.Cursor.Row])
-	}
-}
-
-// CursorPageUp moves the cursor one screen up
-func (e *Editor) CursorPageUp(screenRows int, rowOffset int) {
-	e.Cursor.Row = rowOffset
-	for j := 0; j < screenRows; j++ {
-		e.CursorUp()
-	}
-}
-
-// CursorPageDown moves the cursor one screen down
-func (e *Editor) CursorPageDown(screenRows int, rowOffset int) {
-	e.Cursor.Row = rowOffset + screenRows - 1
-	if e.Cursor.Row > len(e.Rows) {
-		e.Cursor.Row = len(e.Rows)
-	}
-	for j := 0; j < screenRows; j++ {
-		e.CursorDown()
-	}
-}
-
-// CursorHome moves the cursor to col 0
-func (e *Editor) CursorHome() {
-	e.Cursor.Col = 0
-}
-
-// ResetX sets the cursor X position to a valid position after moving y
-func (e *Editor) ResetX() {
-
-	// if we moved past last row, set cursor to 0
-	if e.Cursor.Row >= len(e.Rows) {
-		e.Cursor.Col = 0
-		return
-	}
-
-	// we allow moving to 1 pos past the last character
-	rowLen := len(e.Rows[e.Cursor.Row])
-	if e.Cursor.Col > rowLen {
-		e.Cursor.Col = len(e.Rows[e.Cursor.Row])
-	}
-}
-
-// InsertChar inserts a character at a given location
-func (e *Editor) InsertChar(c rune) {
-
-	// if we're at the last line, insert a new row
-	if e.Cursor.Row == len(e.Rows) {
-		e.InsertRow(len(e.Rows), "")
-	}
-
-	// store a reference to the working row to improve readability
-	src := e.Rows[e.Cursor.Row]
-
-	dest := make([]rune, len(src)+1)
-	copy(dest, src[:e.Cursor.Col])
-	copy(dest[e.Cursor.Col+1:], src[e.Cursor.Col:])
-	dest[e.Cursor.Col] = c
-
-	e.Rows[e.Cursor.Row] = dest
-	e.Dirty = true
-	e.Cursor.Col++
-}
-
-// InsertRow inserts a row at a given index
-func (e *Editor) InsertRow(rowidx int, s string) {
-	if rowidx < 0 || rowidx > len(e.Rows) {
-		return
-	}
-
-	row := []rune(s)
-
-	e.Rows = append(e.Rows, ERow{})
-	copy(e.Rows[rowidx+1:], e.Rows[rowidx:])
-	e.Rows[rowidx] = row
-
-	e.Dirty = true
-}
-
-// InsertNewline inserts a new line at the cursor position
-func (e *Editor) InsertNewline() {
-	if e.Cursor.Col == 0 {
-		e.InsertRow(e.Cursor.Row, "")
-		e.Dirty = true
-		return
-	}
-
-	moveChars := string(e.Rows[e.Cursor.Row][e.Cursor.Col:])
-	e.Rows[e.Cursor.Row] = e.Rows[e.Cursor.Row][:e.Cursor.Col]
-	e.InsertRow(e.Cursor.Row+1, moveChars)
-	e.Dirty = true
-
-	e.Cursor.Row++
-	e.Cursor.Col = 0
-}
-
-// DelChar deletes a character at current cursor location
-func (e *Editor) DelChar() {
-	// if the cursor is at the line beyond the end of text
-	// then move it to the last line
-	if e.Cursor.Row == len(e.Rows) {
-		if len(e.Rows) == 0 {
-			return
-		}
-		e.Cursor.Row = len(e.Rows) - 1
-		e.Cursor.Col = len(e.Rows[e.Cursor.Row])
-	}
-
-	// if at the beginning of the text, then do nothing
-	if e.Cursor.Col == 0 && e.Cursor.Row == 0 {
-		return
-	}
-
-	// different handling for at the beginning of the line or middle of line
-	if e.Cursor.Col > 0 {
-		row := e.Rows[e.Cursor.Row]
-		copy(row[e.Cursor.Col-1:], row[e.Cursor.Col:])
-		row = row[:len(row)-1]
-		e.Rows[e.Cursor.Row] = row
-		e.Cursor.Col--
-	} else {
-		e.Cursor.Col = len(e.Rows[e.Cursor.Row-1])
-		e.Rows[e.Cursor.Row-1] = append(e.Rows[e.Cursor.Row-1], e.Rows[e.Cursor.Row]...)
-		e.DelRow(e.Cursor.Row)
-		e.Cursor.Row--
-	}
-	e.Dirty = true
-}
-
-// DelRow deletes a given row
-func (e *Editor) DelRow(rowidx int) {
-	if rowidx < 0 || rowidx >= len(e.Rows) {
-		return
-	}
-
-	copy(e.Rows[rowidx:], e.Rows[rowidx+1:])
-	e.Rows = e.Rows[:len(e.Rows)-1]
-	e.Dirty = true
-}
-
-// Find searches the whole editor for a text
-func (e *Editor) Find(search ERow) []Point {
-	ret := []Point{}
-
-	for i := 0; i < len(e.Rows); i++ {
-		if idx := runes.Index(runes.ToLower(e.Rows[i]), runes.ToLower(search)); idx != -1 {
-			ret = append(ret, Point{Col: idx, Row: i})
-		}
-	}
-	return ret
-}
-
-// SetCursor sets the cursor to a specific point
-func (e *Editor) SetCursor(p Point) {
-	e.Cursor.Col = p.Col
-	e.Cursor.Row = p.Row
-}
--- listman.go
+++ listman.go
@@ -13,6 +13,27 @@ import (
 func ctrlKey(b byte) rune {
   return rune(b & 0x1f)
 }
+z0 := struct{}{}
+navigation := map[int]struct{} {
+                   ARROW_UP:z0,
+                   ARROW_DOWN:z0,
+                   ARROW_LEFT:z0,
+                   ARROW_RIGHT:z0,
+                  'h':z0,
+                  'j':z0,
+                  'k':z0,
+                  'l':z0,
+                  }
+
+ //type Editor struct{ ...}
+// cmd1_map = make(map[string]func(*Editor, int),4)
+cmd1_map := map[string]func(*Editor, int){
+                   "i":(*Editor).E_i,
+                   "I":(*Editor).E_a,
+                   "a":(*Editor).E_a,
+                   "A":(*Editor).E_A,
+                 }
+// to call it's cmd1_map["i"](e, repeat)
 
 // SafeExit restores terminal using the original terminal config stored
 // in the global session variable
@@ -78,21 +99,325 @@ func main_() {
 	}
 }
 
-func organizerProcessKey(o *Organizer) {
+func organizerProcessKey(c int) {
+
 	switch o.mode {
 
-	case insert:
-	case normal:
-	case commandLine:
+    case NO_ROWS:
+      switch c {
+      case ':':
+        colon_N()
+        return
+      case '\x1b':
+        org.command = ""
+        org.repeat = 0
+        return
+      case 'i', 'I', 'a', 'A', 's':
+        org.insertRow(0, "", true, false, false, BASE_DATE)
+        org.mode = INSERT
+        org.command = ""
+        org.repeat = 0
+        return
+      }
+      return
+
+   	case INSERT:
+      switch c {
+      case ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT:
+        org.moveCursor(c)
+        return
+        case '\x1b':
+          org.command = ""
+          org.mode = NORMAL
+          if org.fc > 0 {
+            org.fc--
+          }
+          sess.showOrgMessage("")
+          return
+        default:
+          org.insertChar(c)
+          return
+        }
+      }
+
+  	case NORMAL:
+      if c == '\x1b' {
+        if (org.view == TASK) {
+          sess.drawPreviewWindow(org.rows.at(org.fr).id)
+        }
+        sess.showOrgMessage("")
+        org.command[0] = ""
+        org.repeat = 0
+        return
+      }
+
+      /*leading digit is a multiplier*/
+      //if (isdigit(c))  //equiv to if (c > 47 && c < 58)
+
+      if ((c > 47 && c < 58) && len(org.command) == 0)) {
+
+        if (org.repeat == 0 && c == 48) {
+
+        } else if org.repeat == 0 {
+          org.repeat = c - 48
+          return
+        } else {
+          org.repeat = org.repeat*10 + c - 48
+          return
+        }
+      }
+
+      if org.repeat == 0 {
+        org.repeat = 1
+      }
+
+      org.command += string(c)
+
+      if cmd, found := n_lookup[org.command] found {
+        cmd()
+        org.command = ""
+        org.repeat = 0
+        return
+      }
+
+      //also means that any key sequence ending in something
+      //that matches below will perform command
+
+      // needs to be here because needs to pick up repeat
+      //Arrows + h,j,k,l
+      if _, found := navigation[c] {
+        for j := 0; j < org.repeat; j++ {
+          org.moveCursor(c)
+        }
+        org.command =  ""
+        org.repeat = 0
+        return
+      }
+
+      return // end of case NORMAL 
+
+  	case COMMAND_LINE:
+      if c == '\x1b' {
+          org.mode = NORMAL
+          sess.showOrgMessage("")
+          return
+      }
+
+      if c == '\r' {
+        pos := strings.Index(org.command_line, ' ')
+        cmd := org.command_line[0:pos]
+        if cmd, found := cmd_lookup[cmd]; found  {
+          if pos == -1 {
+            pos = 0
+          }
+          cmd(pos)
+          return
+        }
+
+        sess.showOrgMessage("\x1b[41mNot an outline command: %s\x1b[0m", cmd.c_str())
+        org.mode = NORMAL
+        return
+      }
+
+      if c == DEL_KEY || c == BACKSPACE {
+        length = len(org.command_line)
+        if length > 0 {
+          org.command_line = org.command_line[:length-1]
+      } else {
+        org.command_line += string(c)
+      }
+
+      sess.showOrgMessage(":%s", org.command_line.c_str())
+      return //end of case COMMAND_LINE
 
 	}
 
-func editorProcessKey(o *Editor) {
-	switch o.mode {
+func editorProcessKey(c int) bool {
 
-	case insert:
-	case normal:
-	case commandLine:
+	switch sess.p.mode {
+
+    case NO_ROWS:
+      switch c {
+      case '\x1b':
+        sess.p.command = ""
+        sess.p.repeat = 0
+        return false
+      case ':':
+        sess.p.mode = COMMAND_LINE
+        sess.p.command_line = ""
+        sess.p.command = ""
+        sess.p.editorSetMessage(":")
+       return false
+      case 'i', 'I', 'a', 'A', 's', 'o', 'O':
+        //p.editorInsertRow(0, std::string())
+        sess.p.mode = INSERT
+        sess.p.last_command = "i" //all the commands equiv to i
+        sess.p.prev_fr = 0
+        sess.p.prev_fc = 0
+        sess.p.last_repeat = 1
+        sess.p.snapshot = nil
+        sess.p.snapshot = append(sess.p.snapshot, "")
+        sess.p.editorSetMessage("\x1b[1m-- INSERT --\x1b[0m")
+        //p.command[0] = '\0'
+        //p.repeat = 0
+        // ? p.redraw = true
+        return true
+      }
+    case INSERT:
+      switch c {
+
+        case '\r':
+          sess.p.editorInsertReturn()
+          sess.p.last_typed += c
+          return true
+
+        // not sure this is in use
+        case ctrlKey('s')
+          sess.p.editorSaveNoteToFile("lm_temp")
+          return false
+
+        case HOME_KEY:
+          sess.p.editorMoveCursorBOL()
+          return false
+
+        case END_KEY:
+          sess.p.editorMoveCursorEOL()
+          sess.p.editorMoveCursor(ARROW_RIGHT)
+          return false
+
+        case BACKSPACE:
+          sess.p.editorBackspace()
+
+          //not handling backspace correctly
+          //when backspacing deletes more than currently entered text
+          //A common case would be to enter insert mode  and then just start backspacing
+          //because then dotting would actually delete characters
+          //I could record a \b and then handle similar to handling \r
+          if (!sess.p.last_typed.empty()) sess.p.last_typed.pop_back()
+          return true
+    
+        case DEL_KEY:
+          sess.p.editorDelChar()
+          return true
+    
+        case ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT:
+          sess.p.editorMoveCursor(c)
+          return false
+    
+        case ctrlKey('b'):
+        //case ctrlKey('i'): ctrlKey('i') -> 9 same as tab
+        case ctrlKey('e'):
+          sess.p.push_current() //p.editorCreateSnapshot()
+          sess.p.editorDecorateWord(c)
+          return true
+    
+        // this should be a command line command
+        case ctrlKey('z'):
+          sess.p.smartindent = (sess.p.smartindent) ? 0 : SMARTINDENT;
+          sess.p.editorSetMessage("smartindent = %d", sess.p.smartindent) 
+          return false
+    
+        case '\x1b':
+
+          /*
+           * below deals with certain NORMAL mode commands that
+           * cause entry to INSERT mode includes dealing with repeats
+           */
+
+          //i,I,a,A - deals with repeat
+          if(cmd_map1.contains(sess.p.last_command)) { 
+            sess.p.push_current() //
+            for (int n=0; n<sess.p.last_repeat-1; n++) {
+              for (char const &c : sess.p.last_typed) {sess.p.editorInsertChar(c)}
+            }
+          }
+
+          //cmd_map2 -> E_o_escape and E_O_escape - here deals with deals with repeat > 1
+          if (cmd_map2.contains(sess.p.last_command)) {
+            (sess.p.*cmd_map2.at(sess.p.last_command))(sess.p.last_repeat - 1)
+            sess.p.push_current()
+          }
+
+          //cw, caw, s
+          if (cmd_map4.contains(sess.p.last_command)) {
+            sess.p.push_current()
+          }
+          //'I' in VISUAL BLOCK mode
+          if (sess.p.last_command == "VBI") {
+            for (int n=0; n<sess.p.last_repeat-1; n++) {
+              for (char const &c : sess.p.last_typed) {sess.p.editorInsertChar(c)}
+            }
+            int temp = sess.p.fr
+
+            for (sess.p.fr=sess.p.fr+1; sess.p.fr<sess.p.vb0[1]+1; sess.p.fr++) {
+              for (int n=0; n<sess.p.last_repeat; n++) { //NOTICE not p.last_repeat - 1
+                sess.p.fc = sess.p.vb0[0] 
+                for (char const &c : sess.p.last_typed) {sess.p.editorInsertChar(c)}
+              }
+            }
+            sess.p.fr = temp
+            sess.p.fc = sess.p.vb0[0]
+          }
+
+          //'A' in VISUAL BLOCK mode
+          if (sess.p.last_command == "VBA") {
+            for (int n=0; n<sess.p.last_repeat-1; n++) {
+              for (char const &c : sess.p.last_typed) {sess.p.editorInsertChar(c);}
+            }
+            //{ 12302020
+            int temp = sess.p.fr
+
+            for (sess.p.fr=sess.p.fr+1; sess.p.fr<sess.p.vb0[1]+1; sess.p.fr++) {
+              for (int n=0; n<sess.p.last_repeat; n++) { //NOTICE not p.last_repeat - 1
+                int size = sess.p.rows.at(sess.p.fr).size()
+                if (sess.p.vb0[2] > size) sess.p.rows.at(sess.p.fr).insert(size, sess.p.vb0[2]-size, ' ')
+                sess.p.fc = sess.p.vb0[2]
+                for (char const &c : sess.p.last_typed) {sess.p.editorInsertChar(c)}
+              }
+            }
+            sess.p.fr = temp
+            sess.p.fc = sess.p.vb0[0]
+          //} 12302020
+          }
+
+          /*Escape whatever else happens falls through to here*/
+          sess.p.mode = NORMAL
+          sess.p.repeat = 0
+
+          //? redundant - see 10 lines below
+          sess.p.last_typed = std::string() 
+
+          if (sess.p.fc > 0) sess.p.fc--
+
+          // below - if the indent amount == size of line then it's all blanks
+          // can hit escape with p.row == NULL or p.row[p.fr].size == 0
+          if (!sess.p.rows.empty() && sess.p.rows[sess.p.fr].size()) {
+            int n = sess.p.editorIndentAmount(sess.p.fr)
+            if (n == sess.p.rows[sess.p.fr].size()) {
+              sess.p.fc = 0
+              for (int i = 0; i < n; i++) {
+                sess.p.editorDelChar()
+              }
+            }
+          }
+          sess.p.editorSetMessage("") // commented out to debug push_current
+          //editorSetMessage(p.last_typed.c_str())
+          sess.p.last_typed.clear()//////////// 09182020
+          return true //end case x1b:
+    
+        // deal with tab in insert mode - was causing segfault  
+        case '\t':
+          for (int i=0; i<4; i++) sess.p.editorInsertChar(' ')
+          return true  
+
+        default:
+          sess.p.editorInsertChar(c)
+          sess.p.last_typed += c
+          return true
+     
+      } //end inner switch for outer case INSERT
+
+      return true // end of case INSERT: - should not be executed
 
 	}
 }
