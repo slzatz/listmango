5510b865a8947f0d1103a148f4422f01283eba75
slzatz <slzatz@gmail.com>
2021-04-13 17:40:38 -0400

    organizer_normal: cleaned up naming of organizer normal functions

--- organizer_normal.go
+++ organizer_normal.go
@@ -1,80 +1,69 @@
 package main
 
 var n_lookup = map[string]func(){
+	"i":   _i,
+	"s":   _s,
+	"~":   tilde,
+	"r":   replace,
+	"a":   _a,
+	"A":   _A,
+	"x":   _x,
+	"w":   _w,
+	"caw": _caw,
+	"cw":  _cw,
+	"daw": _daw,
+	"dw":  _dw,
+	"de":  _de,
+	"d$":  _dEnd,
+	"dd":  del,
+	"gg":  _gg,
+	"b":   _b,
+	"e":   _e,
+	"0":   _zero,
+	"$":   _end,
+	"I":   _I,
+	"G":   _G,
+	":":   exCmd,
+	"v":   _v,
+	"p":   _p,
+	"*":   _asterisk,
+	"m":   mark,
+	"n":   _n,
+
 	string(ctrlKey('l')):       switchToEditorMode,
 	string([]byte{0x17, 0x17}): switchToEditorMode,
-	//  "\r": return_N, //return_O
-	"i": insert,
-	"s": substitute,
-	"~": tilde,
-	"r": replace,
-	"a": appnd,
-	"A": Appnd,
-	"x": delchars,
-	"w": w_N,
-
-	"daw": daw,
-	"dw":  dw_N,
-	"caw": caw_N,
-	"cw":  cw_N,
-	"de":  de_N,
-	"d$":  d_dollar_N,
-
-	"gg": gg_N,
-
-	//"gt": gt_N,
-
-	string(ctrlKey('i')): info_N, //{{0x9}}
-	"b":                  b_N,
-	"e":                  e_N,
-	"0":                  zero_N,
-	"$":                  dollar_N,
-	"I":                  I_N,
-	"G":                  G_N,
-	":":                  exCmd,
-	"v":                  v_N,
-	"p":                  p_N,
-	"*":                  asterisk_N,
-	"m":                  m_N,
-	"n":                  n_N,
-	//"u": u_N,
-	"dd":         dd_N,
-	string(0x4):  dd_N,        //ctrl-d
-	string(0x2):  star_N,      //ctrl-b -probably want this go backwards (unimplemented) and use ctrl-e for this
-	string(0x18): completed_N, //ctrl-x
+	string(0x4):                del,           //ctrl-d
+	string(0x2):                starEntry,     //ctrl-b -probably want this go backwards (unimplemented) and use ctrl-e for this
+	string(0x18):               completeEntry, //ctrl-x
+	string(ctrlKey('i')):       entryInfo,     //{{0x9}}
 }
 
-//case 'i':
-func insert() {
+func _i() {
 	org.mode = INSERT
 	sess.showOrgMessage("\x1b[1m-- INSERT --\x1b[0m")
 }
 
-//case 's':
-func substitute() {
+func _s() {
 	for i := 0; i < org.repeat; i++ {
 		org.delChar()
 	}
-	//row.dirty = true; //in org.delChar but not sure it should be
 	org.mode = INSERT
-	sess.showOrgMessage("\x1b[1m-- INSERT --\x1b[0m") //[1m=bold
+	sess.showOrgMessage("\x1b[1m-- INSERT --\x1b[0m")
 }
 
-//case 'x':
-func delchars() {
+func _x() {
 	for i := 0; i < org.repeat; i++ {
 		org.delChar()
 	}
-	//row.dirty = true;
 }
 
-func daw() {
+func _daw() {
 	for i := 0; i < org.repeat; i++ {
 		org.delWord()
 	}
 }
 
-func caw_N() {
+func _caw() {
 	for i := 0; i < org.repeat; i++ {
 		org.delWord()
 	}
@@ -82,7 +71,7 @@ func caw_N() {
 	sess.showOrgMessage("\x1b[1m-- INSERT --\x1b[0m")
 }
 
-func dw_N() {
+func _dw() {
 	for j := 0; j < org.repeat; j++ {
 		start := org.fc
 		org.moveEndWord2()
@@ -93,7 +82,7 @@ func dw_N() {
 	}
 }
 
-func cw_N() {
+func _cw() {
 	for j := 0; j < org.repeat; j++ {
 		start := org.fc
 		org.moveEndWord2()
@@ -106,7 +95,7 @@ func cw_N() {
 	sess.showOrgMessage("\x1b[1m-- INSERT --\x1b[0m")
 }
 
-func de_N() {
+func _de() {
 	start := org.fc
 	org.moveEndWord() //correct one to use to emulate vim
 	end := org.fc
@@ -115,65 +104,56 @@ func de_N() {
 	*t = (*t)[:org.fc] + (*t)[end:]
 }
 
-func d_dollar_N() {
+func _dEnd() {
 	org.deleteToEndOfLine()
 }
 
-//case 'r':
 func replace() {
 	org.mode = REPLACE
 }
 
-//case '~'
 func tilde() {
 	for i := 0; i < org.repeat; i++ {
 		org.changeCase()
 	}
 }
 
-//case 'a':
-func appnd() {
+func _a() {
 	org.mode = INSERT //this has to go here for MoveCursor to work right at EOLs
 	org.moveCursor(ARROW_RIGHT)
 	sess.showOrgMessage("\x1b[1m-- INSERT --\x1b[0m")
 }
 
-//case 'A':
-func Appnd() {
+func _A() {
 	org.moveCursorEOL()
 	org.mode = INSERT //needs to be here for movecursor to work at EOLs
 	org.moveCursor(ARROW_RIGHT)
 	sess.showOrgMessage("\x1b[1m-- INSERT --\x1b[0m")
 }
 
-//case 'b':
-func b_N() {
+func _b() {
 	org.moveBeginningWord()
 }
 
-//case 'e':
-func e_N() {
+func _e() {
 	org.moveEndWord()
 }
 
-//case '0':
-func zero_N() {
+func _zero() {
 	org.fc = 0 // this was commented out - not sure why but might be interfering with O.repeat
 }
 
-//case '$':
-func dollar_N() {
+func _end() {
 	org.moveCursorEOL()
 }
 
-//case 'I':
-func I_N() {
+func _I() {
 	org.fc = 0
 	org.mode = INSERT
 	sess.showOrgMessage("\x1b[1m-- INSERT --\x1b[0m")
 }
 
-func gg_N() {
+func _gg() {
 	org.fc = 0
 	org.rowoff = 0
 	org.fr = org.repeat - 1 //this needs to take into account O.rowoff
@@ -189,8 +169,7 @@ func gg_N() {
 	}
 }
 
-//case 'G':
-func G_N() {
+func _G() {
 	org.fc = 0
 	org.fr = len(org.rows) - 1
 	if org.view == TASK {
@@ -205,7 +184,6 @@ func G_N() {
 	}
 }
 
-//case ':':
 func exCmd() {
 	sess.showOrgMessage(":")
 	org.command_line = ""
@@ -213,73 +191,55 @@ func exCmd() {
 	org.mode = COMMAND_LINE
 }
 
-//case 'v':
-func v_N() {
+func _v() {
 	org.mode = VISUAL
 	org.highlight[0] = org.fc
 	org.highlight[1] = org.fc
 	sess.showOrgMessage("\x1b[1m-- VISUAL --\x1b[0m")
 }
 
-//case 'p':
-func p_N() {
+func _p() {
 	if len(org.string_buffer) > 0 {
 		org.pasteString()
 	}
 }
 
-//case '*':
-func asterisk_N() {
+func _asterisk() {
 	org.getWordUnderCursor()
 	org.findNextWord()
 }
 
-//case 'm':
-func m_N() {
+func mark() {
 
 	if _, found := org.marked_entries[org.rows[org.fr].id]; found {
 		delete(org.marked_entries, org.rows[org.fr].id)
 	} else {
 		org.marked_entries[org.rows[org.fr].id] = struct{}{}
 	}
-
-	/*
-	  org.rows[org.fr].marked = !org.rows[org.fr].marked
-	  if org.rows[org.fr].marked {
-	    org.marked_entries[org.rows[org.fr].id] = struct{}{}
-	  } else {
-	    delete(org.marked_entries, org.rows[org.fr].id)
-	  }
-	*/
-
 	sess.showOrgMessage("Toggle mark for item %d", org.rows[org.fr].id)
 }
 
-//case 'n':
-func n_N() {
+func _n() {
 	org.findNextWord()
 }
 
-//dd and 0x4 -> ctrl-d
-func dd_N() {
+func del() {
 	toggleDeleted()
 }
 
-//0x2 -> ctrl-b
-func star_N() {
+func starEntry() {
 	toggleStar()
 }
 
-//0x18 -> ctrl-x
-func completed_N() {
+func completeEntry() {
 	toggleCompleted()
 }
 
-func w_N() {
+func _w() {
 	org.moveNextWord()
 }
 
-func info_N() {
+func entryInfo() {
 	e := getEntryInfo(getId())
 	sess.displayEntryInfo(&e)
 	sess.drawPreviewBox()
